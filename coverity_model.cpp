54e9d8f8856b72b6f2b0409c6276ef2c54a5391c
Jan Willamowius <jan@willamowius.de>
2017-10-18 17:20:00 +0200

    model assertions

--- coverity_model.cpp
+++ coverity_model.cpp
@@ -11,6 +11,45 @@ class ios : public _ios_fields{
 	
 };
 
-#define PLDAP_ATTR_INIT(cls, typ, nam, val) cls::typ nam = val;
+enum PStandardAssertMessage {
+  PLogicError,
+  POutOfMemory,
+  PNullPointerReference,
+  PInvalidCast, 
+  PInvalidArrayIndex,
+  PInvalidArrayElement,
+  PStackEmpty,
+  PUnimplementedFunction,
+  PInvalidParameter,
+  POperatingSystemError,
+  PChannelNotOpen,
+  PUnsupportedFeature,
+  PInvalidWindow,
+  PMaxStandardAssertMessage
+};
+
+inline bool PAssertFuncInline(bool b, const char * file, int line, const char * className, PStandardAssertMessage msg)
+{
+	if (!b) {
+		__coverity_panic__();
+	}
+	return b;
+};
+
+bool PAssertFunc(bool b, const char * file, int line, const char * className, const char * msg)
+{
+	if (!b) {
+		__coverity_panic__();
+	}
+	return b;
+};
+
+void PAssertFunc(const char * file, int line, const char * className, const char * msg)
+{
+	__coverity_panic__();
+};
+
+
+#define PLDAP_ATTR_INIT(cls, typ, nam, val) ((cls)::(typ) (nam) = (val));
 #define PAssertAlways(msg)	 __coverity_panic__();
 
