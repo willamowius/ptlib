6defc0089527c6a799fd49c54d15d0a6239c8ce6
Jan Willamowius <jan@willamowius.de>
2018-09-18 21:21:27 +0200

    fix for clang on FreeBSD

--- include/ptlib/object.h
+++ include/ptlib/object.h
@@ -1011,7 +1011,7 @@ struct PAllocatorTemplate
                    + __GNUC_PATCHLEVEL__)
 
 // Memory pooling allocators
-#if defined(__GNUC__) && (GCC_VERSION > 40000) && !defined(P_MINGW) && !defined(P_MACOSX) 
+#if defined(__GNUC__) && (GCC_VERSION > 40000) && !defined(P_MINGW) && !defined(P_MACOSX) && !defined(__clang__)
 #include <ext/mt_allocator.h>
 template <class Type> struct PFixedPoolAllocator    : public PAllocatorTemplate<__gnu_cxx::__mt_alloc<Type>, Type> { };
 template <class Type> struct PVariablePoolAllocator : public PAllocatorTemplate<__gnu_cxx::__mt_alloc<Type>, Type> { };
