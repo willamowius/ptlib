d73595e5a639c349ad2e79c0c243cf856f277c9b
Jan Willamowius <jan@willamowius.de>
2019-02-19 16:03:01 +0100

    init all members, whitespace

--- plugins/sound_pulse/sound_pulse.cxx
+++ plugins/sound_pulse/sound_pulse.cxx
@@ -30,7 +30,7 @@
  * $Author$
  * $Date$
  */
-   
+
 
 #pragma implementation "sound_pulse.h"
 
@@ -39,7 +39,6 @@
 #include <pulse/introspect.h>
 #include <pulse/thread-mainloop.h>
 #include <pulse/volume.h>
-//#include <pulse/gccmacro.h>
 #include <pulse/sample.h>
 #include <ptlib.h>
 #include <ptclib/random.h>
@@ -56,24 +55,24 @@ static pa_context* context;
 class PulseContext {
 private:
   static void notify_cb(pa_context *c,void *userdata) {
-    pa_threaded_mainloop_signal(paloop,0);
+    pa_threaded_mainloop_signal(paloop, 0);
   }
 public:
   PulseContext() {
-    paloop=pa_threaded_mainloop_new();
+    paloop = pa_threaded_mainloop_new();
     pa_threaded_mainloop_start(paloop);
     pa_threaded_mainloop_lock(paloop);
-    pa_proplist *proplist=pa_proplist_new();
+    pa_proplist *proplist = pa_proplist_new();
     pa_proplist_sets(proplist,"media.role","phone");
     /* TODO: I wasn't able to make module-cork-music-on-phone do what I expected */
-    context=pa_context_new_with_proplist(pa_threaded_mainloop_get_api(paloop),"ptlib",proplist);
+    context = pa_context_new_with_proplist(pa_threaded_mainloop_get_api(paloop), "ptlib", proplist);
     pa_proplist_free(proplist);
-    pa_context_connect(context,NULL,PA_CONTEXT_NOFLAGS ,NULL);
-    pa_context_set_state_callback(context,notify_cb,NULL);
-    while (pa_context_get_state(context)<PA_CONTEXT_READY) {
+    pa_context_connect(context, NULL, PA_CONTEXT_NOFLAGS, NULL);
+    pa_context_set_state_callback(context, notify_cb, NULL);
+    while (pa_context_get_state(context) < PA_CONTEXT_READY) {
       pa_threaded_mainloop_wait(paloop);
     }
-    pa_context_set_state_callback(context,NULL,NULL);
+    pa_context_set_state_callback(context, NULL, NULL);
     pa_threaded_mainloop_unlock(paloop);
   }
   ~PulseContext() {
@@ -83,7 +82,7 @@ public:
     pa_threaded_mainloop_free(paloop);
   }
   static void signal() {
-    pa_threaded_mainloop_signal(paloop,0);
+    pa_threaded_mainloop_signal(paloop, 0);
   }
 
 };
@@ -109,10 +108,10 @@ public:
     if (!operation)
       return false;
 
-    while (pa_operation_get_state(operation)==PA_OPERATION_RUNNING) {
+    while (pa_operation_get_state(operation) == PA_OPERATION_RUNNING) {
       pa_threaded_mainloop_wait(paloop);
     }
-    bool toReturn=pa_operation_get_state(operation)==PA_OPERATION_DONE;
+    bool toReturn=pa_operation_get_state(operation) == PA_OPERATION_DONE;
     pa_operation_unref(operation);
     return toReturn;
   }
@@ -149,7 +148,17 @@ void PSoundChannelPulse::Construct()
   PTRACE(6, "Pulse\tConstruct ");
   os_handle = -1;
   s = NULL;
-  ss.format =  PA_SAMPLE_S16LE;
+  ss.format = PA_SAMPLE_S16LE;
+  mNumChannels = 0;
+  mSampleRate = 0;
+  mBitsPerSample = 0;
+  actualSampleRate = 0;
+  direction = Player;
+  resampleRate = 0;
+  bufferSize = 0;
+  bufferCount = 0;
+  record_data = NULL;
+  record_len = 0;
 }
 
 
@@ -176,7 +185,7 @@ static void source_info_cb(pa_context *c, const pa_source_info *i, int eol, void
     PulseContext::signal();
   } else {
     /* Ignore monitor sources */
-    if (i->monitor_of_sink==PA_INVALID_INDEX)
+    if (i->monitor_of_sink == PA_INVALID_INDEX)
       ((PStringArray*) userdata)->AppendString(i->name);
   }
 }
@@ -188,12 +197,12 @@ PStringArray PSoundChannelPulse::GetDeviceNames(Directions dir)
   PStringArray devices;
   devices.AppendString("PulseAudio"); // Default device
   pa_operation* operation;
-  if (dir==Player) {
+  if (dir == Player) {
     operation=
-      pa_context_get_sink_info_list(context,sink_info_cb,&devices);
+      pa_context_get_sink_info_list(context, sink_info_cb, &devices);
   } else {
     operation=
-      pa_context_get_source_info_list(context,source_info_cb,&devices);
+      pa_context_get_source_info_list(context, source_info_cb, &devices);
   }
   lock.waitFor(operation);
   return devices;
@@ -213,7 +222,7 @@ static void stream_notify_cb(pa_stream *s, void *userdata) {
   PulseContext::signal();
 }
 
-static void stream_write_cb(pa_stream* s,size_t nbytes,void *userdata) {
+static void stream_write_cb(pa_stream* s, size_t nbytes, void *userdata) {
   PulseContext::signal();
 }
 
@@ -239,24 +248,21 @@ PBoolean PSoundChannelPulse::Open(const PString & _device,
     appName << app;
   else
     appName << "PTLib plugin ";
-  if (_dir == Player) 
-    streamName << ::hex << PRandom::Number();
-  else
-    streamName << ::hex << PRandom::Number();
+  streamName << ::hex << PRandom::Number();
 
   ss.rate = _sampleRate;
   ss.channels = _numChannels;
-  ss.format =  PA_SAMPLE_S16LE;  
+  ss.format = PA_SAMPLE_S16LE;
 
   const char* dev;
-  if (_device=="PulseAudio") {
+  if (_device == "PulseAudio") {
     /* Default device */
-    dev=NULL;
+    dev = NULL;
   } else {
-    dev=_device;
+    dev = _device;
   }
-  s=pa_stream_new(context,appName.GetPointer(),&ss,NULL);
-  pa_stream_set_state_callback(s,stream_notify_cb,NULL);
+  s = pa_stream_new(context,appName.GetPointer(), &ss, NULL);
+  pa_stream_set_state_callback(s, stream_notify_cb, NULL);
 
   if (s == NULL) {
     PTRACE(2, ": pa_stream_new() failed: " << pa_strerror(pa_context_errno(context)));
@@ -267,34 +273,34 @@ PBoolean PSoundChannelPulse::Open(const PString & _device,
   }
 
   if (_dir == Player) {
-    int err=pa_stream_connect_playback(s,dev,NULL,PA_STREAM_NOFLAGS,NULL,NULL);
+    int err = pa_stream_connect_playback(s, dev, NULL, PA_STREAM_NOFLAGS, NULL, NULL);
     if (err) {
       PTRACE(2, ": pa_connect_playback() failed: " << pa_strerror(err));
       pa_stream_unref(s);
-      s=NULL;
+      s = NULL;
       return PFalse;
     }
-    pa_stream_set_write_callback(s,stream_write_cb,NULL);
+    pa_stream_set_write_callback(s, stream_write_cb, NULL);
   } else {
-    int err=pa_stream_connect_record(s,dev,NULL,PA_STREAM_NOFLAGS);
+    int err = pa_stream_connect_record(s, dev, NULL, PA_STREAM_NOFLAGS);
     if (err) {
       PTRACE(2, ": pa_connect_record() failed: " << pa_strerror(pa_context_errno(context)));
       pa_stream_unref(s);
-      s=NULL;
+      s = NULL;
       return PFalse;
     }
-    pa_stream_set_read_callback(s,stream_write_cb,NULL);
+    pa_stream_set_read_callback(s, stream_write_cb, NULL);
     /* No input yet */
-    record_len=0;
-    record_data=NULL;
+    record_len = 0;
+    record_data = NULL;
   }
 
   /* Wait for stream to become ready */
-  while (pa_stream_get_state(s)<PA_STREAM_READY) lock.wait();
-  if (pa_stream_get_state(s)!=PA_STREAM_READY) {
+  while (pa_stream_get_state(s) < PA_STREAM_READY) lock.wait();
+  if (pa_stream_get_state(s) != PA_STREAM_READY) {
     PTRACE(2, "stream state is " << pa_stream_get_state(s));
     pa_stream_unref(s);
-    s=NULL;
+    s = NULL;
     return PFalse;
   }
 
@@ -332,25 +338,25 @@ PBoolean PSoundChannelPulse::Write(const void * buf, PINDEX len)
   PWaitAndSignal m(deviceMutex);
   PTRACE(6, "Pulse\tWrite " << len << " bytes");
   PulseLock lock;
-  char* buff=(char*) buf;
+  char* buff = (char*) buf;
 
   if (!os_handle) {
     PTRACE(4, ": Pulse audio Write() failed as device closed");
     return PFalse;
   }
 
-  size_t toWrite=len;
+  size_t toWrite = len;
   while (toWrite) {
     size_t ws;
-    while ((ws=pa_stream_writable_size(s))<=0) lock.wait();
-    if (ws>toWrite) ws=toWrite;
-    int err=pa_stream_write(s,buff,ws,NULL,0,PA_SEEK_RELATIVE);
+    while ((ws = pa_stream_writable_size(s)) <= 0) lock.wait();
+    if (ws > toWrite) ws = toWrite;
+    int err = pa_stream_write(s, buff, ws, NULL, 0, PA_SEEK_RELATIVE);
     if (err) {
       PTRACE(4, ": pa_stream_write() failed: " << pa_strerror(err));
       return PFalse;
     }
-    toWrite-=ws;
-    buff+=ws;
+    toWrite -= ws;
+    buff += ws;
   }
 
   lastWriteCount = len;
@@ -364,26 +370,26 @@ PBoolean PSoundChannelPulse::Read(void * buf, PINDEX len)
   PWaitAndSignal m(deviceMutex);
   PTRACE(6, "Pulse\tRead " << len << " bytes");
   PulseLock lock;
-  char* buff=(char*) buf;
+  char* buff = (char*) buf;
 
   if (!os_handle) {
     PTRACE(4, ": Pulse audio Read() failed as device closed");
     return PFalse;
   }
 
-  size_t toRead=len;
+  size_t toRead = len;
   while (toRead) {
     while (!record_len) {
       /* Fill the record buffer first */
       pa_stream_peek(s,&record_data,&record_len);
       if (!record_len) lock.wait();
     }
-    size_t toCopy=toRead<record_len ? toRead : record_len;
-    memcpy(buff,record_data,toCopy);
-    toRead-=toCopy;
-    buff+=toCopy;
-    record_data=((char*) record_data)+toCopy;
-    record_len-=toCopy;
+    size_t toCopy = toRead < record_len ? toRead : record_len;
+    memcpy(buff, record_data, toCopy);
+    toRead -= toCopy;
+    buff += toCopy;
+    record_data = ((char*) record_data)+toCopy;
+    record_len -= toCopy;
     /* Buffer empty? */
     if (!record_len) pa_stream_drop(s);
   }
@@ -451,7 +457,6 @@ PBoolean PSoundChannelPulse::GetBuffers(PINDEX & size, PINDEX & count)
 
 PBoolean PSoundChannelPulse::PlaySound(const PSound & sound, PBoolean wait)
 {
-
   return PFalse;
 }
 
@@ -515,16 +520,16 @@ PBoolean PSoundChannelPulse::WaitForAllRecordBuffersFull()
   return PFalse;
 }
 
-static void sink_volume_cb(pa_context* context,const pa_sink_info* i,int eol,void* userdata) {
+static void sink_volume_cb(pa_context* context, const pa_sink_info* i, int eol, void* userdata) {
   if (!eol) {
-    *((pa_cvolume*) userdata)=i->volume;
+    *((pa_cvolume*) userdata) = i->volume;
     PulseContext::signal();
   }
 }
 
-static void source_volume_cb(pa_context* context,const pa_source_info* i,int eol,void* userdata) {
+static void source_volume_cb(pa_context* context, const pa_source_info* i, int eol, void* userdata) {
   if (!eol) {
-    *((pa_cvolume*) userdata)=i->volume;
+    *((pa_cvolume*) userdata) = i->volume;
     PulseContext::signal();
   }
 }
@@ -533,44 +538,40 @@ PBoolean PSoundChannelPulse::SetVolume(unsigned newVal)
 {
   if (s) {
     PulseLock lock;
-    int dev=pa_stream_get_device_index(s);
-    pa_operation* operation;
+    int dev = pa_stream_get_device_index(s);
+    pa_operation* operation = NULL;
     pa_cvolume volume;
-    if (direction==Player) {
-      operation=pa_context_get_sink_info_by_index(context,dev,sink_volume_cb,&volume);
+    if (direction == Player) {
+      operation = pa_context_get_sink_info_by_index(context, dev, sink_volume_cb, &volume);
     } else {
-      operation=pa_context_get_source_info_by_index(context,dev,source_volume_cb,&volume);
+      operation = pa_context_get_source_info_by_index(context, dev, source_volume_cb, &volume);
     }
     if (!lock.waitFor(operation)) return PFalse;
-    pa_cvolume_scale(&volume,newVal*PA_VOLUME_NORM/100);
-    if (direction==Player) {
-      pa_context_set_sink_volume_by_index(context,dev,&volume,NULL,NULL);
+    pa_cvolume_scale(&volume,newVal*PA_VOLUME_NORM / 100);
+    if (direction == Player) {
+      pa_context_set_sink_volume_by_index(context, dev, &volume, NULL, NULL);
     } else {
-      pa_context_set_source_volume_by_index(context,dev,&volume,NULL,NULL);
+      pa_context_set_source_volume_by_index(context, dev, &volume, NULL, NULL);
     }
   }
   return PTrue;
 }
 
-PBoolean  PSoundChannelPulse::GetVolume(unsigned &devVol)
+PBoolean  PSoundChannelPulse::GetVolume(unsigned & devVol)
 {
   if (s) {
     PulseLock lock;
-    int dev=pa_stream_get_device_index(s);
-    pa_operation* operation;
+    int dev = pa_stream_get_device_index(s);
+    pa_operation* operation = NULL;
     pa_cvolume volume;
-    if (direction==Player) {
-      operation=pa_context_get_sink_info_by_index(context,dev,sink_volume_cb,&volume);
+    if (direction == Player) {
+      operation=pa_context_get_sink_info_by_index(context, dev, sink_volume_cb, &volume);
     } else {
-      operation=pa_context_get_source_info_by_index(context,dev,source_volume_cb,&volume);
+      operation=pa_context_get_source_info_by_index(context, dev, source_volume_cb, &volume);
     }
     if (!lock.waitFor(operation)) return PFalse;
-    devVol=100*pa_cvolume_avg(&volume)/PA_VOLUME_NORM;
+    devVol = 100 * pa_cvolume_avg(&volume) / PA_VOLUME_NORM;
   }
   return PTrue;
 }
-  
-
-
-// End of file
-
+ 
