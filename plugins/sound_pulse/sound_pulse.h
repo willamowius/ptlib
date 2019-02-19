2a8e03df73099991cbeefcd910b1751c72507f14
Jan Willamowius <jan@willamowius.de>
2019-02-19 16:02:36 +0100

    remove unused member

--- plugins/sound_pulse/sound_pulse.h
+++ plugins/sound_pulse/sound_pulse.h
@@ -82,7 +82,6 @@ class PSoundChannelPulse: public PSoundChannel
     unsigned actualSampleRate;
     Directions direction;
     PString device;
-    PBoolean isInitialised;
     unsigned resampleRate;
 
     PINDEX bufferSize;
