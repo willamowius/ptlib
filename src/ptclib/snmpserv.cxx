2a0e4118783d4b1e916e412d00624edd18b86d43
Jan Willamowius <jan@willamowius.de>
2019-05-26 23:18:40 +0200

    support SNMP SET

--- src/ptclib/snmpserv.cxx
+++ src/ptclib/snmpserv.cxx
@@ -99,7 +99,7 @@ PBoolean PSNMPServer::HandleChannel()
 
 			// if the buffer was too small, then we are receiving datagrams
 			// and the datagram was too big
-			if (PChannel::GetErrorCode() == PChannel::BufferTooSmall) 
+			if (PChannel::GetErrorCode() == PChannel::BufferTooSmall)
 				lastErrorCode = RxBufferTooSmall;
 			else
 				lastErrorCode = NoResponse;
@@ -109,7 +109,7 @@ PBoolean PSNMPServer::HandleChannel()
 			} else if ((rxSize + GetLastReadCount()) >= 10)
 			break;
 
-			else 
+			else
 			rxSize += GetLastReadCount();
 		}
 
@@ -191,8 +191,8 @@ static void DecodeOID(const PDUType & pdu, PINDEX & reqID, PSNMP::BindingList & 
 }
 
 template <typename PDUType>
-static void EncodeOID(PDUType & pdu, const PINDEX & reqID, 
-					  const PSNMP::BindingList & varlist, 
+static void EncodeOID(PDUType & pdu, const PINDEX & reqID,
+					  const PSNMP::BindingList & varlist,
 					  const PSNMP::ErrorType & errCode)
 {
    pdu.m_request_id = reqID;
@@ -219,7 +219,7 @@ PBoolean PSNMPServer::MIB_LocalMatch(PSNMP_PDU & pdu)
   PBoolean found = PFalse;
   PSNMP_VarBindList & vars = pdu.m_variable_bindings;
   PINDEX size = vars.GetSize();
- 
+
   for(PINDEX x = 0 ;x < size; x++){
     PRFC1155_ObjectSyntax *obj = (PRFC1155_ObjectSyntax*) objList.GetAt(vars[x].m_name);
     if (obj != NULL){
@@ -247,7 +247,7 @@ PBoolean PSNMPServer::ProcessPDU(const PBYTEArray & readBuffer, PBYTEArray & sen
 {
 
   PSNMP_Message msg;
-  
+
   if (!msg.Decode((PASN_Stream &)readBuffer)) {
 	  PTRACE(4,"SNMPsrv\tERROR DECODING PDU");
 	  return PFalse;
@@ -290,13 +290,13 @@ PBoolean PSNMPServer::ProcessPDU(const PBYTEArray & readBuffer, PBYTEArray & sen
         }
         resp.Encode((PASN_Stream &)sendBuffer);
       }
-      
+
     break;
     case PSNMP_PDUs::e_get_next_request:
       DecodeOID<PSNMP_GetNextRequest_PDU>(msg.m_pdu, reqID, varlist);
       retval = OnGetNextRequest(reqID,varlist,errCode);
       if (retval == PTrue){
-        sendpdu.SetTag(PSNMP_PDUs::e_get_response);   
+        sendpdu.SetTag(PSNMP_PDUs::e_get_response);
         EncodeOID<PSNMP_GetResponse_PDU>(sendpdu, reqID,varlist,errCode);
         resp.m_pdu = sendpdu;
         resp.m_version = msg.m_version;
@@ -313,6 +313,19 @@ PBoolean PSNMPServer::ProcessPDU(const PBYTEArray & readBuffer, PBYTEArray & sen
     case PSNMP_PDUs::e_set_request:
       DecodeOID<PSNMP_SetRequest_PDU>(msg.m_pdu, reqID, varlist);
       retval = OnSetRequest(reqID, varlist, errCode);
+      if (retval == PTrue){
+        sendpdu.SetTag(PSNMP_PDUs::e_get_response); // yes, a SET gets a get-response
+        EncodeOID<PSNMP_GetResponse_PDU>(sendpdu, reqID,varlist,errCode);
+        resp.m_pdu = sendpdu;
+        resp.m_version = msg.m_version;
+        resp.m_community = msg.m_community;
+        PSNMP_GetResponse_PDU & mpdu = (PSNMP_GetResponse_PDU &) resp.m_pdu;
+        if (!MIB_LocalMatch(mpdu)){
+          retval = PFalse;
+          break;
+        }
+        resp.Encode((PASN_Stream &)sendBuffer);
+      }
       break;
 
     case PSNMP_PDUs::e_get_response:
